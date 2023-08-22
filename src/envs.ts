0e77453792a67125b8b1a5655f75db0426eca015
iamtomorrow <tomorrow2001@outlook.com.br>
2023-08-21 22:06:38 -0300

    committing updates from aug 21 for deployment

--- src/envs.ts
+++ src/envs.ts
@@ -4,4 +4,4 @@ dotenv.config( );
 
 export const SERVER_PORT = process.env.PORT;
 export const BASE_URL = process.env.BASE_URL
-export const MONGODB_DATABASE_URL = process.env.MONGO_DB_DATABASE_PORT
+export const MONGODB_DATABASE_URL = process.env.MONGODB_URI as string 
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,17 @@
 
 import multer from 'multer';
 import { SERVER_PORT, MONGODB_DATABASE_URL } from './envs';
-import cors, { CorsOptions, CorsOptionsDelegate } from 'cors';
+import cors from 'cors';
 import mongoose from 'mongoose';
 import { router } from './router';
 import dotenv from 'dotenv';
-import express, { NextFunction, Response, Request } from 'express';
+import express from 'express';
 import path from 'path';
-import { request } from 'http';
 
 const server = express( );
 server.use(cors());
 
-dotenv.config( );
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
 mongoose.connect(MONGODB_DATABASE_URL as string);
 mongoose.connection.on("error", ( ) => {
